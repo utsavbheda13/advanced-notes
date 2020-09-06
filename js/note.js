c3a4f7d1771e228795dc872ab0ca9c7625b352e0
Utsav <63046322+utsavbheda13@users.noreply.github.com>
2020-09-06 17:04:56 +0530

    LAB 7
    Now same theme has applied to all pages and some bootstrap classes have been used.

--- js/note.js
+++ js/note.js
@@ -1,10 +1,19 @@
-function saveNote(){
-    var mydb = document.getElementById("store-note");
-    var content=document.getElementById("note").value;
-    if(content != ""){
-        var para=document.createElement("p");
-        var node=document.createTextNode(content);
-        para.appendChild(node);
-        mydb.appendChild(para);
-    }
+// function saveNote(){
+//     var mydb = document.getElementById("store-note");
+//     var content=document.getElementById("note").value;
+//     if(content != ""){
+//         var para=document.createElement("p");
+//         var node=document.createTextNode(content);
+//         para.appendChild(node);
+//         mydb.appendChild(para);
+//     }
+// }
+
+function addNote() {
+    //document.getElementById("exampleModal").Modal('hide');
+    //document.getElementById("test").appendChild(document.getElementById("addtitle").appendChild(document.createTextNode(document.getElementById("exampleModalLabel").value)));
+}
+
+function delNote() {
+    document.getElementById("card1").remove();
 }
